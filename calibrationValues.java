456d788335578a987d3783ff591f320ee310debb
Sarah Cadet <scadet@bu.edu>
2023-12-03 12:04:59 -0500

    first commit; day 1 completed; day 2 in progress

---
+++ calibrationValues.java
@@ -0,0 +1,94 @@
+public class calibrationValues {
+  public static String[] wordNums = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+  public static void main(String[] args) {
+
+  }
+
+  public static int computeSum(String s) {
+    int start = 0;
+    int end = s.indexOf('\n');
+    int sum = 0;
+    while (end != -1) {
+      sum += computeLine(start, end, s);
+      start = end + 1;
+      end = s.indexOf('\n', end + 1);
+    }
+    return sum;
+  }
+
+  public static int computeLine(int start, int end, String s) {
+    String num = "";
+    Integer firstNum = null;
+    Integer lastNum = null;
+    String temp = s.substring(start, end);
+    char[] sArr = temp.toCharArray();
+    boolean firstFound = false;
+    int i = 0;
+    for (char c : sArr) {
+      if (isDigit(i, temp)) {
+        String digit = "";
+        if (Character.isDigit(c)) {
+          digit += c;
+        } else {
+          digit += getNumFromWord(temp, i);
+        }
+        if (!firstFound) {
+          firstNum = Integer.valueOf("" + digit);
+          firstFound = true;
+        }
+        lastNum = Integer.valueOf("" + digit);
+      }
+      i++;
+    }
+    int result = Integer.valueOf(num + firstNum + lastNum);
+    System.out.println(result);
+    return result;
+  }
+
+  public static boolean isDigit(int i, String s) {
+    String temp = s.substring(i);
+    if (Character.isDigit(temp.charAt(0)))
+      return true;
+    else {
+      for (String num : wordNums) {
+        if (temp.startsWith(num)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  public static int getNumFromWord(String s, int i) {
+    String temp = s.substring(i);
+    String num = "";
+    for (String n : wordNums) {
+      if (temp.startsWith(n))
+        num = n;
+    }
+    switch (num) {
+      case "one":
+        return 1;
+      case "two":
+        return 2;
+      case "three":
+        return 3;
+      case "four":
+        return 4;
+      case "five":
+        return 5;
+      case "six":
+        return 6;
+      case "seven":
+        return 7;
+      case "eight":
+        return 8;
+      case "nine":
+        return 9;
+      default:
+        System.out.println("HELP!" + num);
+        return 0;
+    }
+  }
+}
---
+++ cubesInBag.java
@@ -0,0 +1,83 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/*
+ * The Elf would first like to know which games would have been possible 
+ * if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
+ */
+public class cubesInBag {
+  public static void main(String[] args) {
+    File input = new File("./inputCubes.txt");
+    Scanner inputFileReader = null;
+    try {
+      inputFileReader = new Scanner(input);
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    System.out.println(sumOfValidGameIds(inputFileReader));
+  }
+
+  public static int sumOfValidGameIds(Scanner fR) {
+    if (fR == null) {
+      System.out.println("NULL File Reader !!!");
+      return 0;
+    }
+    int sum = 0;
+    while (fR.hasNextLine()) {
+      String line = fR.nextLine();
+      line = removeCommasAndColon(line);
+      sum += isValidId(line);
+    }
+    return sum;
+  }
+
+  public static String removeCommasAndColon(String line) {
+    String result = "";
+    for (char c : line.toCharArray()) {
+      if (c != ',' && c != ':') {
+        result += c;
+      }
+    }
+    return result;
+  }
+
+  public static int isValidId(String line) {
+    int val = 0;
+    HashMap<String, Integer> count = new HashMap<>();
+    count.put("red", 0);
+    count.put("blue", 0);
+    count.put("green", 0);
+    // string array; loop through for keywords and use that to add appropriate
+    // values
+    String[] lineSplit = line.split(" ");
+    for (int i = 1; i < lineSplit.length; i++) {
+      String amount = lineSplit[i - 1];
+      switch (lineSplit[i]) {
+        case "red":
+          Integer curr1 = count.get("red");
+          count.replace("red", curr1 + Integer.valueOf(amount));
+          break;
+        case "blue":
+          Integer curr2 = count.get("blue");
+          count.replace("blue", curr2 + Integer.valueOf(amount));
+          break;
+        case "green":
+          Integer curr3 = count.get("green");
+          count.replace("green", curr3 + Integer.valueOf(amount));
+          break;
+      }
+    }
+    if (possible(count.get("red"), count.get("green"), count.get("blue"))) {
+      val = Integer.valueOf(lineSplit[1]);
+    }
+    return val;
+  }
+
+  public static boolean possible(Integer i, Integer j, Integer k) {
+    return i <= 12 && j <= 13 && k <= 14;
+  }
+}
