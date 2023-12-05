de0731ea16dfc0121f4dfebe7130ba57bddfb3a8
Sarah Cadet <scadet@bu.edu>
2023-12-05 00:05:17 -0500

    part1day3 solved

--- day3/machinePartsDay3Part1.java
+++ day3/machinePartsDay3Part1.java
@@ -1,5 +1,6 @@
 package day3;
 
+import java.util.Arrays;
 /**
  * machinePartsDay3Part1
  */
@@ -30,32 +31,97 @@ public class machinePartsDay3Part1 {
     int sum = 0;
     // make the matrix
     char[][] matrix = new char[140][140];
-    for (int i = 0; i < 140; i++) {
+    for (int i = 0; i < matrix.length; i++) {
       matrix[i] = reader.nextLine().toCharArray();
-      for (int j = 0; j < 140; j++) {
+    }
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
         if (Character.isDigit(matrix[i][j])) {
-          sum += evaluateNum(matrix, i, j);
+          int start = j;
+          String numString = "";
+          while (j < matrix[0].length && Character.isDigit(matrix[i][j])) {
+            numString += matrix[i][j];
+            j++;
+          }
+          int num = Integer.valueOf(numString);
+          int numLength = j - start;
+          sum += evaluateNum(matrix, i, start, num, numLength);
         }
       }
     }
     return sum;
   }
 
-  public static int evaluateNum(char[][] matrix, int i, int j) {
+  public static int evaluateNum(char[][] matrix, int i, int start, int num, int numLength) {
     int value = 0;
-    int start = j;
-    String numString = "";
-    while (Character.isDigit(matrix[i][j])) {
-      numString += matrix[i][j];
-      j++;
+    if (isAPart(matrix, i, start, numLength)) {
+      value = num;
     }
-    int num = Integer.valueOf(numString);
-    int numLength = j - start;
-    if (isAPart(matrix, i, start, numLength))
-      return value;
+    return value;
   }
 
   public static boolean isAPart(char[][] matrix, int row, int startingColOfNum, int length) {
+    boolean up = false, down = false, left = false, right = false, diag = false;
+    if (row >= 1) {
+      diag = checkLeft(matrix, row - 1, startingColOfNum, length);
+      if (!diag) {
+        diag = checkRight(matrix, row - 1, startingColOfNum, length);
+      }
+      up = checkUp(matrix, row, startingColOfNum, length);
+    }
+    if (row < matrix.length - 1) {
+      if (!diag) {
+        diag = checkLeft(matrix, row + 1, startingColOfNum, length);
+      }
+      if (!diag) {
+        diag = checkRight(matrix, row + 1, startingColOfNum, length);
+      }
+      down = checkDown(matrix, row, startingColOfNum, length);
+    }
+    if (startingColOfNum + length < matrix[0].length) {
+      right = checkRight(matrix, row, startingColOfNum, length);
+    }
+    if (startingColOfNum > 0) {
+      left = checkLeft(matrix, row, startingColOfNum, length);
+    }
+    return up || down || left || right || diag;
+  }
 
+  public static boolean checkUp(char[][] matrix, int row, int startingColOfNum, int length) {
+    int upperbound = startingColOfNum + length;
+    for (int i = startingColOfNum; i < upperbound; i++) {
+      if (isASymbol(matrix[row - 1][i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static boolean checkDown(char[][] matrix, int row, int startingColOfNum, int length) {
+    int upperbound = startingColOfNum + length;
+    for (int i = startingColOfNum; i < upperbound; i++) {
+      if (isASymbol(matrix[row + 1][i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static boolean checkRight(char[][] matrix, int row, int startingColOfNum, int length) {
+    if (startingColOfNum + length >= matrix[0].length) {
+      return false;
+    }
+    return isASymbol(matrix[row][startingColOfNum + length]);
+  }
+
+  public static boolean checkLeft(char[][] matrix, int row, int startingColOfNum, int length) {
+    if (startingColOfNum == 0) {
+      return false;
+    }
+    return isASymbol(matrix[row][startingColOfNum - 1]);
+  }
+
+  public static boolean isASymbol(char c) {
+    return !Character.isDigit(c) && c != '.';
   }
 }
